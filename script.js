bb2425f7dd7e9837d42363172bfa577b55983f74
Ritesh <ritz2020j@gmail.com>
2025-08-06 13:51:15 +0530

    Add getComputerChoice function with switch-case and random logic

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+function getComputerChoice(){
+   let result = Math.floor(Math.random() * 3);
+
+    switch(result){
+        case 0 : 
+            return "rock";
+        case 1 :
+             return "paper";
+        case 2 :
+             return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
