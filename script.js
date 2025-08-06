927c3e9931553669a41f72ee225d1dc1fa47453c
Ritesh <ritz2020j@gmail.com>
2025-08-06 15:13:57 +0530

    Add playRound function with win/draw/lose logic and score update

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function getComputerChoice(){
         case 1 :
              return "paper";
         case 2 :
-             return "scissors";
+             return "scissor";
     }
 }
 
@@ -26,3 +26,50 @@ function getHumanChoice(){
 }
 
 // console.log(getHumanChoice());
+
+function playRound(humanChoice,computerChoice){
+   
+    // humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice){
+        console.log("It's a draw!!");
+        return 
+    }
+
+    else if(humanChoice === "rock" && computerChoice === "scissor"){
+        humanScore += 1;
+        console.log("You Win!! rock beats scissor")
+    }
+
+    else if(humanChoice === "rock" && computerChoice === "paper"){
+        computerScore += 1;
+        console.log("You Lose!! paper beats rock")
+    }
+
+    else if(humanChoice === "paper" && computerChoice === "rock"){
+        humanScore += 1;
+        console.log("You Win!! paper beats rock")
+    }
+
+    else if(humanChoice === "paper" && computerChoice === "scissor"){
+        computerScore += 1;
+        console.log("You lose!! scissor beats paper")
+    }
+
+    else if(humanChoice === "scissor" && computerChoice === "paper"){
+        humanScore += 1;
+        console.log("You Win!! scissor beats paper")
+    }
+
+    else if(humanChoice === "scissor" && computerChoice === "rock"){
+        humanScore += 1;
+        console.log("You lose!! rock beats scissor")
+    }
+}
+
+const human = getHumanChoice();
+const computer = getComputerChoice();
+
+playRound(human,computer);
+
+console.log(`scores -> Human: ${humanScore},Computer: ${computerScore}`)
